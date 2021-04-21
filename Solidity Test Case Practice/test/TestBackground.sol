f73db708899656dfad51e2d8f81b702195c3c09e
kenta0558 <mannheim42@yahoo.de>
2021-04-21 23:47:31 +0200

    add new file TestEntryPoint.sol with testConstructorAndGetBackgroundAddress into tests

--- Solidity Test Case Practice/test/TestBackground.sol
+++ Solidity Test Case Practice/test/TestBackground.sol
@@ -18,21 +18,21 @@ contract TestBackground {
         uint value = 5;
         background.storeValue(value);
         uint result = background.getValue(0);
-        assert.equal(result, value, "store correct value with storeValue function");
+        Assert.equal(result, value, "store correct value with storeValue function");
     }
 
     function testNumberOfValues() public {
         uint value = 33;
         background.storeValue(value);
         uint numberOfValues = background.getNumberOfValues();
-        assert.equal(numberOfValues, 1, "get correct length of values array");
+        Assert.equal(numberOfValues, 1, "get correct length of values array");
     }
 
     function testStoreTwoValues() public {
         for (uint i = 0; i < 10; i++) {
             background.storeValue(i);
             uint result = background.getValue(i);
-            assert.equal(i, result, "store multiple values correct with storeValue function"); 
+            Assert.equal(i, result, "store multiple values correct with storeValue function"); 
         }    
     }
 }
---
+++ Solidity Test Case Practice/test/TestEntryPoint.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >0.7.0 <0.9.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Background.sol";
+import "../contracts/EntryPoint.sol";
+
+
+contract TestEntryPoint {
+
+    function testConstructorAndGetBackgroundAddress() {
+        
+        Background background = new Background();
+
+        EntryPoint entryPoint = new EntryPoint(address(background));
+
+        address expected = address(background);
+        address backgroundAddress = entryPoint.getBackgroundAddress();
+        
+        Assert.equal(expected, backgroundAddress, "set correct address for EntryPoint contract");
+    }
+}
+
+// contract TestIntegrationEntryPoint {
+
+//     BackgroundTest public backgroundTest;
+// }
+
