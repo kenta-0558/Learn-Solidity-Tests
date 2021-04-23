3a86ad3051111e377c38b564fef24c65eb55d29c
kenta0558 <mannheim42@yahoo.de>
2021-04-23 14:24:19 +0200

    add new integration test EntryPoint into EntryPoint.test.js to test functions storeTwoValues and getNumberOfValues

--- Solidity Test Case Practice/test/EntryPoint.test.js
+++ Solidity Test Case Practice/test/EntryPoint.test.js
@@ -1,1 +1,16 @@
-contract EntryPoint = artifacts.require("../contracts/EntryPoint.sol");
+const EntryPoint = artifacts.require("../contracts/EntryPoint.sol");
+
+contract("EntryPoint", accounts => {
+    describe("Storing Values", () => {
+        it("Stores correctly", async() => {
+            const entryPoint = await EntryPoint.deployed();
+
+            let numberOfValues = await entryPoint.getNumberOfValues();
+            numberOfValues.toString().should.equal("0");
+
+            await entryPoint.storeTwoValues(2, 4);
+            numberOfValues = await entryPoint.getNumberOfValues();
+            numberOfValues.toString().should.equal("2");
+        })    
+    })
+})
