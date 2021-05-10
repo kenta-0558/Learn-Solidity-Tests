87442abb4b00b00fb8c36b10d640c1caa5a8d968
kenta0558 <mannheim42@yahoo.de>
2021-05-10 17:09:49 +0200

    add comment to the file with name of account owner

--- Web3/Intro to Web3.js Ethereum Bockchain Developer Crash Course/1.js
+++ Web3/Intro to Web3.js Ethereum Bockchain Developer Crash Course/1.js
@@ -4,6 +4,8 @@ const rpuURL = "https://mainnet.infura.io/v3/cf78b0d0d1b6495abc36b4d30ef932ca";
 
 const web3 = new Web3(rpuURL);
 
+
+// address of vitalik buterin
 const address = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
 
 web3.eth.getBalance(address, (err, wei) => {
