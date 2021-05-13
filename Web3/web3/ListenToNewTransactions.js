80bb9e21121f4e87d042671cec11ad6518699f5f
kenta0558 <mannheim42@yahoo.de>
2021-05-13 19:31:35 +0200

    addd new file ListenToNewTransactions.js to check current block

---
+++ Web3/web3/ListenToNewTransactions.js
@@ -0,0 +1,21 @@
+const Web3 = require('web3');
+const web3 = new Web3("https://mainnet.infura.io/v3/cf78b0d0d1b6495abc36b4d30ef932ca");
+
+let latestKnownBlockNumber = -1;
+let blockTime = 5000;
+
+const processBlock = async (blockNumber) => {
+    console.log("We process block: " + blockNumber);
+    latestKnownBlockNumber = blockNumber;
+} 
+
+const checkCurrentBlock = async () => {
+    const currentBlockNumber = await web3.eth.getBlockNumber();
+    console.log("Current blockchain top: " + currentBlockNumber, " Script is at: " + latestKnownBlockNumber);
+    while(latestKnownBlockNumber == -1 || currentBlockNumber > latestKnownBlockNumber) {
+        await processBlock(latestKnownBlockNumber == -1 ? currentBlockNumber : latestKnownBlockNumber + 1);     
+    }
+    setTimeout(checkCurrentBlock, blockTime);
+}
+
+checkCurrentBlock();
