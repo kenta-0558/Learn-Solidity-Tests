1d6d7dec8ecef9020b634a4b92464a9e9b509213
kenta0558 <mannheim42@yahoo.de>
2021-05-16 23:20:30 +0200

    add new function getName into Test.sol and migrate to deploy this contract

--- Web3/ContractInstanceWithWeb3/contracts/Test.sol
+++ Web3/ContractInstanceWithWeb3/contracts/Test.sol
@@ -4,9 +4,19 @@ pragma solidity ^0.5.1;
 
 contract Test {
     
-    string name;
+    string name = "Kiichi";
+
+    function getFirstName() public view returns (string memory) {
+        return name;
+    }
+
+    // function getNameWithFamilyName() public view returns (string memory nameWithFirstName) {
+    //     nameWithFirstName = name + " " + "Tsuchiya";
+    //     return nameWithFirstName;
+    // }
 
     function changeName(string memory _newName) public {
         name = _newName;
     }
+
 }
