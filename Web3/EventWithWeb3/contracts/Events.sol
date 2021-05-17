c0d49bb8fd6205b94f58e16659670265a843b6b1
kenta0558 <mannheim42@yahoo.de>
2021-05-17 17:33:07 +0200

    fix bug which came in event

--- Web3/EventWithWeb3/contracts/Events.sol
+++ Web3/EventWithWeb3/contracts/Events.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.5.1;
 
 contract Events {
 
-    event MyEvent {
+    event MyEvent (
         uint indexed id,
         uint indexed date,
         string value
-    };
+    );
 
     uint nextId;
 
